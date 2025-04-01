43c5d46774e7ad3dbbec5dfbb10c529bc7a2015c
Ratanak-IT <pozzvinzin821@gmail.com>
2025-04-01 23:42:18 +0700

    Layout

---
+++ src/component/Layouts/NavbarComponent.jsx
@@ -0,0 +1,23 @@
+import { Navbar, NavbarBrand, NavbarCollapse, NavbarToggle } from "flowbite-react";
+import { NavLink } from "react-router-dom";
+
+export default function NavbarComponent() {
+  return (
+    <Navbar fluid rounded>
+      <NavbarBrand href="https://flowbite-react.com">
+        <img src="/Assets/Nham-Ey_Png.png" className="mr-3 h-6 sm:h-9" alt="Flowbite React Logo" />
+      </NavbarBrand>
+      <NavbarToggle />
+      <NavbarCollapse>
+        <div>
+          <NavLink to="/App" className={({ isActive }) => isActive ? "text-blue-500 font-bold" : ""}>
+            Home
+          </NavLink>
+          <NavLink to="/Foodpage"> Food</NavLink>
+          <NavLink to="/Rasturantpage"> Restaurant </NavLink>
+          <NavLink to="/AboutUSpage"> About Us </NavLink>
+        </div>
+      </NavbarCollapse>
+    </Navbar>
+  );
+}
---
+++ src/pages/Foodpage/Foodpage.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Foodpage() {
+  return (
+    <div>Foodpage</div>
+  )
+}
