8f826ae20f4ff346b76424c73121a191f9e31017
Min Phanith <@example.com>
2025-04-02 02:27:23 +0700

    Add images, implement routing, and create layout components

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,12 @@
 import React from "react";
-import ProductCard from "./component/cards/ProductCard";
+import NavbarComponent from "./component/Layout/NavbarComponent";
+
 
 function App() {
   return (
     <>
-      <h1 className="text-red-600">Hello, world!</h1>
-      <div className="grid grid-cols-4 gap-4">
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        
-      </div>
+      <NavbarComponent />
+      <RasturantCard />
     </>
   );
 }
