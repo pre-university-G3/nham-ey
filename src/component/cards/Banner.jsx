37fc3593fd9facdc24d21e3fa0e301335370f4a3
halabiboii <mayahfun@gmail.com>
2025-04-03 00:11:40 +0700

    add banner

---
+++ src/component/cards/Banner.jsx
@@ -0,0 +1,23 @@
+export default function Banner({image}) {
+    return (
+      <>
+        <div className="mx-auto w-[1200px] my-10 p-10 bg-primary text-white flex items-center justify-between rounded-lg shadow-lg">
+          <div>
+            <h1 className="text-xl font-bold uppercase">
+              Savor the True Taste of{" "}
+              <span className="text-yellow-300">Cambodia Fresh, Flavorful</span>
+            </h1>
+            <p className="text-sm mt-2">Taste the Tradition, Savor the Culture</p>
+          </div>
+          <div className="w-24 mr-20">
+            <img
+              src={image}
+              alt="image"
+              className="w-full h-auto"
+            />
+          </div>
+        </div>
+      </>
+    );
+  }
+  
