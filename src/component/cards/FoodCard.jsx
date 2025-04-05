c4a99629f01265ee8b2186303091c0fc56c57b40
halabiboii <mayahfun@gmail.com>
2025-04-05 21:08:16 +0700

    update

--- src/component/cards/FoodCard.jsx
+++ src/component/cards/FoodCard.jsx
@@ -1,43 +1,3 @@
-<<<<<<< HEAD
-export default function FoodCard({ image_url, name, description, price, average_rating }) {
-  return (
-    <>
-      <a href="">
-        <div className="w-[400px] h-[419px] bg-white rounded-2xl shadow-sm box-border mb-10">
-          <div className="relative px-[20px] py-[20px] bg-[#fafaf9]">
-            <img
-              className="w-[362px] h-[203px] object-cover rounded-t-lg"
-              src={image_url}
-              alt={name}
-            />
-            <button className="absolute top-8 right-8 p-2 rounded-full shadow-md bg-white/20 backdrop-blur-[px]">
-              <svg width="24" height="21" viewBox="0 0 26 24" fill="none" xmlns="http://www.w3.org/2000/svg"></svg>
-            </button>
-          </div>
-          <div className="p-4">
-            <h3 className="text-[24px] font-bold text-[#494949] font-kh line-clamp-1">{name}</h3>
-            <small className="text-sm text-gray-600 mt-1 text-[16px] line-clamp-3 leading-[18px]">
-              {description}
-            </small>
-            
-            <div className="flex items-center justify-between mt-3">
-              <span className="text-[#5B913B] font-bold font-kh text-[24px]">${price}</span>
-              <div className="flex items-center">
-                {/* Star Icon */}
-                <svg
-                  className="w-[26px] h-[26px] text-yellow-300"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="currentColor"
-                  viewBox="0 0 22 20"
-                >
-                  <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-                </svg>
-                <p className="ms-2 text-[20px] font-kh font-bold text-gray-900">
-                  {average_rating ? average_rating.toFixed(1) : "No rating"}
-                </p>
-              </div>
-=======
 "use client"
 
 export default function FoodCard({ id, image_url, name, description, price, average_rating }) {
@@ -70,7 +30,6 @@ export default function FoodCard({ id, image_url, name, description, price, aver
               <p className="ms-2 text-[20px] font-kh font-bold text-gray-900">
                 {average_rating ? average_rating.toFixed(1) : "No rating"}
               </p>
->>>>>>> f98ad53c02e44730c8858a451c5c8dffbb9b4f72
             </div>
           </div>
         </div>
--- src/pages/MenuPage/Rasturant.jsx
+++ src/pages/MenuPage/Rasturant.jsx
@@ -19,28 +19,6 @@ export default function Rasturant() {
   
   const base_url = `https://nham-ey.istad.co`;
 
-<<<<<<< HEAD
-return (
-  <main className="">
-      <div className="text-center text-3xl font-bold flex justify-center items-center  bg-gray-100 py-4 rounded-lg shadow-md mb-20">
-         <h1>Rasturant</h1>
-         <span className="text-primary ml-2">listing</span>
-    </div>
-        <section className="mx-50 grid grid-cols-2  lg:grid-cols-2 gap-5 px-4">
-        {Rasturant.map((Rasturant) => (
-          < RestaurantCard
-            key={Rasturant.id}
-            name={Rasturant.name}
-            image_url={`${base_url}${Rasturant.image_url}`}
-            description={Rasturant.description}
-          />
-        ))}
-        </section>  
-        
-      </main>
-)
-}
-=======
   return (
     <main className="">
       {/* Contained Hero Banner Section */}
@@ -100,4 +78,3 @@ return (
     </main>
   );
 }
->>>>>>> f98ad53c02e44730c8858a451c5c8dffbb9b4f72
